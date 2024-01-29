702e0368e91e15b1e5d33796edd9e0c8339e0c80
madebymak <mr.jmak@gmail.com>
2024-01-29 11:55:52 -0800

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+	theme: {
+		extend: {
+			backgroundImage: {
+				'hero': "url('/src/assets/Coding-Challenge-Image.png')"
+      },
+		},
+		colors: {
+			'maroon': '#611818',
+			'white': '#FFFFFF'
+		},
+		fontFamily: {
+			'lato': ['lato', 'sans-serif'],
+			'didot': ['didot', 'serif']
+		}
   },
   plugins: [],
 }
-
